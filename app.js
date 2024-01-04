96eb0b8be7ca157c705e0a11919a5c7df6b0db6b
200OKHarsh <155547534+200OKHarsh@users.noreply.github.com>
2024-01-04 12:07:26 +0530

    Update app.js

--- app.js
+++ app.js
@@ -15,15 +15,13 @@ app.use(bodyParser.json());
 app.use('/uploads/images', express.static(path.join('uploads', 'images')));
 
 app.use((req, res, next) => {
-  // res.setHeader('Access-Control-Allow-Credentials', true);
-  // res.setHeader('Access-Control-Allow-Origin', '*');
-  //  res.setHeader(
-  //   'Access-Control-Allow-Headers',
-  //   'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-  // );
-  // res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  res.setHeader('Access-Control-Allow-Origin', '*');
+   res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  );
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
   next();
 });
 
