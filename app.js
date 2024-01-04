84238322fc41235218cf2c40678463bce0f4108c
200OKHarsh <155547534+200OKHarsh@users.noreply.github.com>
2024-01-04 13:00:17 +0530

    Update app.js

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.use((req, res, next) => {
     'Access-Control-Allow-Headers',
     'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
   );
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS');
   next();
 });
 
