41398951f68c6578fb240669df0e82d6fe78d2b2
200OKHarsh <155547534+200OKHarsh@users.noreply.github.com>
2024-01-04 13:07:03 +0530

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const userRoutes = require('./router/users-routes');
 const leaveRoutes = require('./router/leave-routes');
 const HttpError = require('./models/http-error');
 const path = require('path');
-
+const cors = require('cors');
 const app = express();
 
 app.use(bodyParser.json());
@@ -20,17 +20,18 @@ const corsOptions = {
   optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
 };
 
+app.use(cors());
 
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Credentials', true);
-   res.setHeader(
-    'Access-Control-Allow-Headers',
-    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-  );
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS');
-  next();
-});
+// app.use((req, res, next) => {
+//   res.setHeader('Access-Control-Allow-Origin', '*');
+//   res.setHeader('Access-Control-Allow-Credentials', true);
+//    res.setHeader(
+//     'Access-Control-Allow-Headers',
+//     'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+//   );
+//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS');
+//   next();
+// });
 
 app.use('/api/users', userRoutes);
 app.use('/api/leave', leaveRoutes);
